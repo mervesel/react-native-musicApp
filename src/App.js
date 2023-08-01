8b135efbf0027b548b0f72be29a43fad6be633b1
mervesel <merveslckoglu3@gmail.com>
2023-08-01 15:41:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React,{useState} from "react";
+import { SafeAreaView,View,FlatList,StyleSheet } from "react-native";
+import music_data from "./music-data.json";
+import SongCard from "./components/SongCard";
+import SearchBar from "./components/SearchBar";
+
+const App=()=>{
+    const [list,setList]=useState(music_data);
+    const [name,setName]=useState(music_data);
+
+    
+    const renderSong=({item})=> <SongCard song= {item} />
+    const renderSeperator=()=> <View style= {styles.seperator} />
+
+    const handleSearch=text=>{
+        const filteredList=music_data.filter(song =>{
+            const searchedText=text.toLowerCase();
+            const currentTitle=song.title.toLowerCase();
+            
+            return currentTitle.indexOf(searchedText) > -1;
+
+        });
+     
+
+        setList(filteredList);
+
+
+    };
+ 
+    return(
+        <SafeAreaView style= {styles.container} >
+            <SearchBar onSearch= {handleSearch}/>
+            <FlatList 
+                keyExtractor={(item)=>item.id}
+                 data= {list}
+                 renderItem={renderSong }
+                 ItemSeparatorComponent={renderSeperator}
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles= StyleSheet.create({
+    container: {
+        flex:1,
+    },
+    seperator: {
+        borderWidth:1,
+        borderColor: '#c0c0c0',
+    }
+})
+
+export default App;
